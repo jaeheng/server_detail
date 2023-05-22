ed4c7f488ca4122a0b741be855080d9006c1bd6c
jaeheng <jaeheng@126.com>
2023-05-22 15:47:49 +0800

    在站点信息卡片上增加插件入口

--- server_detail.php
+++ server_detail.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: 服务器信息
-Version: 1.0.0
+Version: 1.0.1
 Plugin URL:
 Description: 查看服务器详细信息
 Author: jaeheng
@@ -12,8 +12,17 @@ Author URL: https://blog.zhangziheng.com
 
 function server_detail_sidebar()
 {
-    echo '<a class="collapse-item" id="server_detail" href="plugin.php?plugin=server_detail">服务器信息</a>';
+    $url = BLOG_URL . "admin/plugin.php?plugin=server_detail";
+    echo '<a class="collapse-item" id="server_detail" href="' . $url . '">服务器信息</a>';
+}
+
+function server_detail_dashboard()
+{
+    $url = BLOG_URL . "admin/plugin.php?plugin=server_detail";
+    $link = '<a style="float: right;" href="' . $url . '">查看服务器信息</a>';
+    echo "<script>$('.card-header:contains(\"站点信息\")').append('{$link}')</script>";
 }
 
 
 addAction('adm_menu_ext', 'server_detail_sidebar');
+addAction('adm_main_content', 'server_detail_dashboard');
